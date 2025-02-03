d5765ae67f76c5db13e40fcb61080643a3a5fe0a
Vishwalata <vishu92.c@gmail.com>
2025-02-03 07:48:04 +0530

    Assignment3

---
+++ src/com/assignments/Assignment3_Employees.java
@@ -0,0 +1,12 @@
+package com.assignments;
+
+public class Assignment3_Employees {
+	
+	String[] empName = {"Bharath", "ABC", "XYZ"};
+	int[] empIds = {1234, 1235, 1236};
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/com/assignments/Assignments3_EmpGroups.java
@@ -0,0 +1,17 @@
+package com.assignments;
+
+public class Assignments3_EmpGroups {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Assignment3_Employees obj = new Assignment3_Employees();
+		
+		for(int i = 0; i<3; i++)
+		{
+			System.out.println("Employee name : " +obj.empName[i]+ ", Employee ID : " +obj.empIds[i]);
+		}
+		
+
+	}
+
+}
